c649a907aa27fee3337f331f4f8b2b8da833a8ca
Daniel Smither <smitherd9@gmail.com>
2016-07-26 12:23:04 -0400

    Refactored code to use a function

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    // function input() {
+    // 	var number = prompt('Input a number from 1 to 100');
+    // number();
+    // };
+
+    // function input(num) {
+    // 	var int = num;
+    // 	if (int >= 1) {
+    // 		return 
+    // 	}
+    // }
 
 
-var myArray = new Array(100);
+
+    $('.number-entry-form').submit(function(event) {
+        event.preventDefault();
+
+        var input = parseInt($('#number-entry').val(), 10);
+        if (input < 0){
+        	// do something
+        } else {
+        	// do the opposite 
+        }
+        $('.number-entry').val("");
+        // months? 1, 2, 4, Credit card
+        // CVV -> 345.4
+        // 
+        // $('#number-entry').val().parseInt(int);
+        // var int = parseInt(int);
+        // var parse = $('.number-entry').parseInt()
+
+// <p>, <div>, <br/>, <section>
+
+        for (var i = 1; i <= input; i++) {
+
+            if ((i % 3) == 0 && (i % 5) == 0) {
+                $('.results').append(' FIZzBuzZ <br/> ');
+            } else if ((i % 3) == 0) {
+                $('.results').append(' fizz <br/>');
+            } else if ((i % 5) == 0) {
+                $('.results').append(' buzz <br/>');
+            } else
+                $('.results').append(i + '  <br/>');
+        }
 
 
-for (var i = 1; i <= myArray.length; i++) {
-	
-	if ( (i % 3) == 0 && (i % 5) == 0) {
-	 	$('body').append(' FIZzBuzZ ');
-	}
 
-	else if ( ( i % 3) == 0) {
-		$('body').append(' fizz ');
-	}
-
-	else if ( (i % 5) == 0) {
-		$('body').append(' buzz ');
-	}
-
-	else $('body').append(i + ' ');
-	
-
-	
-	
-
-	
-};
+    });
 
 });
